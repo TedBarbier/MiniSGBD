37f96e8d207a67f9af29dadec107f11673d0629b
E4lix <ealix95@gmail.com>
2025-12-11 17:25:58 +0100

    HachageStatique amélioré

--- IndexHachageStatique.py
+++ IndexHachageStatique.py
@@ -13,54 +13,56 @@ def openFile(file_name):
     except Exception as e:
         print(f"Erreur de lecture: {e}")
 
-def hachage_statique(t : Tuple, modulo : int, attribut : int, records_per_block : int):
+def hachage_statique(t : list[Tuple], modulo : int, attribut : int, records_per_block : int):
     # Préouverture des fichiers de sortie blocs
     Table = []
     for i in range(0, modulo):
         file_name = f"table.{i}.bloc0"
         Table.append(openFile(file_name))
-        print(Table[i])
+        # print(Table[i])
     
     # Boucle de traitement Tuple par Tuple
-    index = t.val[attribut] % modulo
     Tableau_verif = [0] * modulo
     Bloc_count = [0] * modulo
 
-    print(f"Index calculé pour le tuple {t.val} : {index}")
+    for element in t:
+        index = element.val[attribut] % modulo # Calcul de l'index de hachage
+        if (Tableau_verif[index] == records_per_block):
+            # Si le bloc est plein, on ferme le fichier courant et on en ouvre un nouveau
+            Table[index].close()
+            Bloc_count[index] += 1
+            print(Bloc_count[index])
+            file_name = f"table.{i}.bloc{Bloc_count[index]}"
+            Table[index] = openFile(file_name)
+            Tableau_verif[index] = 0
 
-    if (Tableau_verif[index] == t.records_per_block):
-        # Si le bloc est plein, on ferme le fichier courant et on en ouvre un nouveau
-        Table[index].close()
-        Bloc_count[index] += 1
-        file_name = f"table.{i}.bloc{Bloc_count[index]}"
-        Table.append(openFile(file_name))
-        Tableau_verif[index] = 0
+            # Puis on écrit le tuple dans le nouveau bloc
+            f = Table[index]
+            for k in range(element.size):
+                value = element.val[k]
+                f.write(bytes(value))
 
-        # Puis on écrit le tuple dans le nouveau bloc
-        f = Table[index]
-        for k in range(t.size):
-            value = t.val[k]
-            f.write(bytes(value))
+            Tableau_verif[index] +=1
+            f.seek(1)
+            f.write(bytes(Tableau_verif[index]))
+            f.seek(0, 2)
 
-        Tableau_verif[index] +=1
-        f.seek(1)
-        f.write(bytes(Tableau_verif[index]))
-        f.seek(0, 2)
+        else:
+            # Écriture du tuple dans le bloc correspondant
+            f = Table[index]
+            for k in range(element.size):
+                value = element.val[k]
+                f.write(bytes(value))
 
-    else:
-        # Écriture du tuple dans le bloc correspondant
-        f = Table[index]
-        for k in range(t.size):
-            value = t.val[k]
-            f.write(bytes(value))
+            print(Tableau_verif[index])
+            Tableau_verif[index] +=1
+            print(Tableau_verif[index])
+            f.seek(1)
+            f.write(bytes(Tableau_verif[index]))
+            f.seek(0, 2)
+            
+            print(f"Tuple {element.val} écrit dans le fichier table.{index}.bloc{Bloc_count[index]}")
 
-        Tableau_verif[index] +=1
-        f.seek(1)
-        f.write(bytes(Tableau_verif[index]))
-        f.seek(0, 2)
-        
-    print(f"Tuple {t.val} écrit dans le fichier table.{index}.bloc0")
-    
     # Fermeture des fichiers
     for i in range(0, modulo):
         Table[i].close()    
--- TestHachageStatique.py
+++ TestHachageStatique.py
@@ -3,21 +3,43 @@ from Tuple import Tuple
 
 # Test avec plusieurs tuples
 tuple1 = Tuple(3)
-tuple1.val = [1, 10, 100]
-hachage_statique(tuple1, 5, 1, records_per_block=4)
+tuple1.val = [1, 24, 100]
 
 tuple2 = Tuple(3)
-tuple2.val = [2, 15, 200]
-hachage_statique(tuple2, 5, 1, records_per_block=4)
+tuple2.val = [2, 31, 200]
 
 tuple3 = Tuple(3)
-tuple3.val = [3, 20, 300]
-hachage_statique(tuple3, 5, 1, records_per_block=4)
+tuple3.val = [3, 28, 300]
 
 tuple4 = Tuple(3)
-tuple4.val = [4, 25, 400]
-hachage_statique(tuple4, 5, 1, records_per_block=4)
+tuple4.val = [4, 16, 400]
 
 tuple5 = Tuple(3)
-tuple5.val = [5, 30, 500]
-hachage_statique(tuple5, 5, 1, records_per_block=4)
+tuple5.val = [5, 2, 500]
+
+tuple6 = Tuple(3)
+tuple6.val = [5, 2, 500]
+
+tuple7 = Tuple(3)
+tuple7.val = [5, 2, 500]
+
+tuple8 = Tuple(3)
+tuple8.val = [5, 2, 500]
+
+tuple9 = Tuple(3)
+tuple9.val = [5, 2, 500]
+
+tuple10 = Tuple(3)
+tuple10.val = [5, 2, 500]
+
+tuple11 = Tuple(3)
+tuple11.val = [5, 2, 500]
+
+tuple12 = Tuple(3)
+tuple12.val = [5, 2, 500]
+
+tuple13 = Tuple(3)
+tuple13.val = [5, 2, 500]
+
+tuples = [tuple1, tuple2, tuple3, tuple4, tuple5, tuple6, tuple7, tuple8, tuple9, tuple10, tuple11, tuple12, tuple13]
+hachage_statique(tuples, modulo=5, attribut=1, records_per_block=3)
